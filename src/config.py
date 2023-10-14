92b5648dc3f4d071ccd05c63d7212b0e0df8c5ba
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2023-10-14 16:44:16 +0300

    Fix config DIRs

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ SCHEDULES_URLS = [
     'https://my.ukma.edu.ua/files/schedule/2023/1/1470/3.doc',
 ]
 
-DOWNLOADED_SCHEDULES_DIR = '../data/downloaded_schedules/'
-PARSED_SCHEDULE_DIR = '../data/'
+DOWNLOADED_SCHEDULES_DIR = 'data/downloaded_schedules/'
+PARSED_SCHEDULE_DIR = 'data/'
